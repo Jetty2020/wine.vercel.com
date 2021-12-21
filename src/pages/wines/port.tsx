fce5b0fb3b3f6286129e5d291886be0f2b781cec
Jetty2020 <kmclo@naver.com>
2021-12-21 21:31:30 +0900

    ✨Port-Wine-Page Wine-API 연동

--- src/pages/wines/port.tsx
+++ src/pages/wines/port.tsx
@@ -1,11 +1,30 @@
-import { NextPage } from "next";
+import type { NextPage } from 'next';
+import { Error, Loading, WineCard } from '../../components';
+import { useWineData } from '../../hooks/useWineData';
+import { Wine } from '../../types/Wine';
 
-const PortPage: NextPage = () => {
+const PortWinePage: NextPage = () => {
+  const name = 'port';
+  const { data, error } = useWineData(name);
+
+  if (error) return <Error />;
+  if (!data) return <Loading />;
+
   return (
     <div>
-      port
+      <h1>Wine</h1>
+      <main>
+        {data.map((wineData: Wine) => {
+          return (
+            <WineCard
+              key={`port-wine-list-${wineData.id}`}
+              wineData={wineData}
+            />
+          );
+        })}
+      </main>
     </div>
   );
-}
+};
 
-export default PortPage;
+export default PortWinePage;
